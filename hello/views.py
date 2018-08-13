8b12ad920a4e3fd37564f22468edbe51eaf7125e
unknown <ASPNET@ZINGHR-136.zinghr.local>
2018-08-13 13:46:35 +0530

    first push
    first push

---
+++ hello/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+from .nlp import sentiment
+
+from .models import Greeting
+
+# Create your views here.
+def index(request):
+    q = request.GET.get('query')
+    print("query= "+q)
+    # q=q.split("/")
+    if q :
+        data = sentiment(q)
+        # print(data)
+        return JsonResponse(data)
+    else:
+        return HttpResponse("Request param expected.")
+
+
+def db(request):
+
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, 'db.html', {'greetings': greetings})
+
+# changes
+
